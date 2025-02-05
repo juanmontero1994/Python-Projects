4d509b6d8fa817bb7b3da17b7151a167fae7e9d5
juanmontero1994 <106863865+juanmontero1994@users.noreply.github.com>
2025-02-05 15:40:13 -0400

    Update LoginProject.py
    This is just a basic Login and Create User Python Program

--- LoginProject.py
+++ LoginProject.py
@@ -1,41 +1,47 @@
 import hashlib
 import os
 
+#Sign-Up Fuction Start Here
 def signup():
     email= input("Enter username: ")
 
-    if os.path.exists("credentials.txt") and os.stat("credentials.txt").st_size>0:
-        with open("credentials.txt", "r") as f:
-            lines= f.read().splitlines()
+    if os.path.exists("credentials.txt") and os.stat("credentials.txt").st_size>0:      #Check if the file exist
+        with open("credentials.txt", "r") as f: #open File credentials.txt
+            lines= f.read().splitlines()    #read lines on the file
 
-        for i in range (0, len(lines), 2):
-            stored_email = lines[i].strip()
+#check if email already exist or not.
+        for i in range (0, len(lines), 2): 
+            stored_email = lines[i].strip()  
             if email == stored_email:
                 print("Username already exist!")
                 return #To exit the fuction if true
     pwd = input("Enter Password: ")
     conf_pwd = input("Comfirm Password ")
 
+ #If both pass entered are the same, this will Hash the Password using SHA256
     if conf_pwd == pwd:
         #Hash the Password using SHA256
         enc = conf_pwd.encode()
         hash1 = hashlib.sha256(enc).hexdigest()
         
-        with open("credentials.txt", "a") as f:
-            f.write(email + "\n")
-            f.write(hash1 + "\n")
+        with open("credentials.txt", "a") as f:   # Open credentials.txt file and append to this.
+            f.write(email + "\n")           #append the email
+            f.write(hash1 + "\n")           #append the password hash
         print("You have registered Successfully!")
     else: 
         print("Password Dont Match")
+#Sign-Up Fuction END Here
 
+#Log-In Fuction Start Here
 def login():
     email = input("Enter Username: ")
     pwd= input("Enter Password: ")
     auth = pwd.encode()
-    auth_hash= hashlib.sha256(auth).hexdigest()
+    auth_hash= hashlib.sha256(auth).hexdigest() #Hash password entered to login
 
+#Split Username from pass
     try:
-        with open("credentials.txt", "r") as f:
+        with open("credentials.txt", "r") as f: #Split Username from pass
             lines= f.read().splitlines()
 
         for i in range (0, len(lines), 2):
@@ -43,17 +49,18 @@ def login():
                 stored_email = lines[i].strip()
                 stored_pwd = lines[i+1].strip()
 
-                if email == stored_email and auth_hash == stored_pwd:
+#Compare both User and Pass to make sure is correct. 
+                if email == stored_email and auth_hash == stored_pwd:  
                     print("Authenticating........\n")
                     print("Logged in!")
                     return
-        print("Wrong !")
+        print("Wrong, !")
 
     except FileNotFoundError:
         print("Something went wrong")
-    
-    ## END login
+## END login
 
+#This is a menu, were we call both fuctions and interact with the code.
 while True: 
     print("-------------- SignUp and Logging Up---------------")
     print("1. Sign-Up")
